b44f3d7f3916ad723449c734d989bfb49f2a3090
Aweroro Hadi <hadistic20@gmail.com>
2025-02-04 23:16:30 +0100

    feat(project): abstracted project items from project section

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -1,31 +1,6 @@
 import ProjectCard from '../components/ProjectCard';
 import { motion } from "framer-motion";
-
-const projects = [
-  {
-    title: "Weather App",
-    description: "A real-time weather forecast app using OpenWeather API and Auth0 for authentication",
-    image:"/src/assets/Weather-app-signed-out.png",
-    techStack: ["Nextjs", "Javascript", "Tailwind CSS", "Auth0"],
-    liveLink: 'https://weather-app-rho-ivory-95.vercel.app/',
-    gitHubLink: 'https://github.com/Aweroro/weather-app'
-  },
-  {
-    title: "Book Tracker",
-    description: "A reading tracker app to manage books across different categories. It uses Firebase for authentication and database",
-    image:'/src/assets/book-reading-app-signed-in.png',
-    techStack: ["Nextjs", "Typescript", "Tailwind CSS", "Firebase"],
-    liveLink: 'https://book-reading-tracker-f73c2.web.app/',
-    gitHubLink: 'https://github.com/Aweroro/book-reading-tracker'
-  },
-  {
-    title: "Form Validation Application",
-    description: "A basic form application that validates and authenticate inputs and submission of a form page using formik and yup.",
-    image:'/src/assets/form-page-app.png',
-    techStack: ["Typescript", "Tailwind CSS", "Formik", "Yup"],
-    gitHubLink: 'https://github.com/Aweroro/form-validation-app'
-  }
-]
+import { projects } from '../components/ProjectItems';
 
 const Projects = () => {
   return (

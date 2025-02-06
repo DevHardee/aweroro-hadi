8da33dce1085c8980f1aab507f38cd3e9c7722a1
Aweroro Hadi <hadistic20@gmail.com>
2025-02-06 19:08:32 +0100

    navbar update and refactoring

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -7,61 +7,63 @@ const NavBar = () => {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <MaxWidthWrapper>
-      <nav className="text-text p-4 shadow-md z-50">
-      <div className="flex justify-between items-center px-2">
-        <h1 className="text-lg md:text-xl lg:text3xl font-bold">Aweroro Hadi</h1>
+    <div className="fixed top-0 left-0 w-full bg-primary shadow-md z-50">
+      <MaxWidthWrapper>
+        <nav className="text-text px-4 py-4">
+          <div className="flex justify-between items-center">
+            <h1 className="text-lg md:text-xl lg:text-3xl font-bold">Aweroro Hadi</h1>
 
-        {/* Mobile Menu Toggle */}
-        <div className="md:hidden">
-          {isOpen ? (
-            <HiX className="text-2xl cursor-pointer" onClick={() => setIsOpen(false)} />
-          ) : (
-            <HiMenu className="text-2xl cursor-pointer" onClick={() => setIsOpen(true)} />
+            {/* Mobile Menu Toggle */}
+            <div className="md:hidden">
+              {isOpen ? (
+                <HiX className="text-2xl cursor-pointer" onClick={() => setIsOpen(false)} />
+              ) : (
+                <HiMenu className="text-2xl cursor-pointer" onClick={() => setIsOpen(true)} />
+              )}
+            </div>
+
+            {/* Desktop Menu */}
+            <div className="hidden md:flex space-x-10 pr-4 font-bold">
+              {['home', 'about', 'projects', 'contact'].map((section) => (
+                <Link
+                  key={section}
+                  to={section}
+                  smooth={true}
+                  duration={500}
+                  className="cursor-pointer pb-2 relative text-md md:text-lg lg:text-xl font-bold 
+                  after:content-[''] after:absolute after:w-full after:scale-x-0 after:h-[2px] 
+                  after:bg-secondary after:bottom-0 after:left-0 after:transition-transform 
+                  after:duration-500 after:origin-left hover:after:scale-x-100"
+                >
+                  {section.charAt(0).toUpperCase() + section.slice(1)}
+                </Link>
+              ))}
+            </div>
+          </div>
+
+          {/* Mobile Menu*/}
+          {isOpen && (
+            <div className="md:hidden flex flex-col items-center text-text py-4 space-y-4">
+              {['home', 'about', 'projects', 'contact'].map((section) => (
+                <Link
+                  key={section}
+                  to={section}
+                  smooth={true}
+                  duration={500}
+                  onClick={() => setIsOpen(false)}
+                  className="cursor-pointer pb-2 relative text-md md:text-lg lg:text-xl font-bold 
+                  after:content-[''] after:absolute after:w-full after:scale-x-0 after:h-[2px] 
+                  after:bg-secondary after:bottom-0 after:left-0 after:transition-transform 
+                  after:duration-500 after:origin-left hover:after:scale-x-100"
+                >
+                  {section.charAt(0).toUpperCase() + section.slice(1)}
+                </Link>
+              ))}
+            </div>
           )}
-        </div>
-
-        {/* Desktop Menu */}
-        <div className="hidden md:flex space-x-10 pr-4 font-bold">
-          {['home', 'about', 'projects', 'contact'].map((section) => (
-            <Link
-              key={section}
-              to={section}
-              smooth={true}
-              duration={500}
-              className="cursor-pointer pb-2 relative text-md md:text-lg lg:text-xl font-bold 
-              after:content-[''] after:absolute after:w-full after:scale-x-0 after:h-[2px] 
-              after:bg-secondary after:bottom-0 after:left-0 after:transition-transform 
-              after:duration-500 after:origin-left hover:after:scale-x-100"
-            >
-              {section.charAt(0).toUpperCase() + section.slice(1)}
-            </Link>
-          ))}
-        </div>
-      </div>
-
-      {/* Mobile Menu (Dropdown) */}
-      {isOpen && (
-        <div className="md:hidden bg-primary flex font-bold flex-col items-center text-text py-4 space-y-4">
-          {['home', 'about', 'projects', 'contact'].map((section) => (
-            <Link
-              key={section}
-              to={section}
-              smooth={true}
-              duration={500}
-              onClick={() => setIsOpen(false)}
-              className="cursor-pointer pb-2 relative text-md md:text-lg lg:text-xl font-bold 
-              after:content-[''] after:absolute after:w-full after:scale-x-0 after:h-[2px] 
-              after:bg-secondary after:bottom-0 after:left-0 after:transition-transform 
-              after:duration-500 after:origin-left hover:after:scale-x-100"
-            >
-              {section.charAt(0).toUpperCase() + section.slice(1)}
-            </Link>
-          ))}
-        </div>
-      )}
-    </nav>
-    </MaxWidthWrapper>
+        </nav>
+      </MaxWidthWrapper>
+    </div>
   );
 };
 
--- src/components/ResumeDownloadCard.tsx
+++ src/components/ResumeDownloadCard.tsx
@@ -4,7 +4,7 @@ const ResumeDownloadCard = () => {
   return (
       <a 
       href='/files/Hadi-resume.pdf'
-      className='font-semibold flex items-center gap-3 mt-4 border border-gray-400 px-4 rounded py-2 hover:border-emerald-300 hover:shadow-lg transition-all'
+      className='font-semibold text-sm md:text-base flex items-center gap-3 mt-4 border border-gray-400 px-4 rounded py-2 hover:border-emerald-300 hover:shadow-lg transition-all'
       >
         Download my resume
         <FaDownload/>
